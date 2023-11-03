33fc72140b45df94e3474d7963de0b981f60b16d
Eric Wang <eric0717@gmail.com>
2023-11-03 15:52:58 -0400

    database setup

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
