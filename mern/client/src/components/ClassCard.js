6015a10aa40318a2e20d2352e903079d4091943e
Amber Lai <alai8@u.rochester.edu>
2023-11-03 18:28:53 -0400

    added animations

--- mern/client/src/components/ClassCard.js
+++ mern/client/src/components/ClassCard.js
@@ -13,17 +13,12 @@ return (
       display: 'block', width: 700, padding: 30 
   }}> 
       <Card> 
-          <CardImg 
-              width="50px"
-              height="50px"
-          src="/Users/amber/Documents/Team-3/mern/client/src/components/GWC_Logo_White.svg"
-              alt="Class Logo" /> 
           <CardBody> 
               <CardTitle tag="h5">{name}</CardTitle> 
               <CardText >{description}</CardText> 
-              <CardText> What is it about: {about} </CardText> 
-              <CardText> Requirements: {requirements} </CardText> 
-              <CardText> Class Schedule: {schedule} </CardText> 
+              <CardText> {about} </CardText> 
+              <CardText> {requirements} </CardText> 
+              <CardText> {schedule} </CardText> 
               <Button className="custom-btn" onClick={expandButton}>Learn More!</Button> 
           </CardBody> 
       </Card> 
