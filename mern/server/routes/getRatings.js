1b4a7b3ac483d598334ecf904c90af63a271b9fc
Eric Wang <eric0717@gmail.com>
2023-11-03 20:04:41 -0400

    better database organization (mongoose)

--- mern/server/routes/getRatings.js
+++ mern/server/routes/getRatings.js
@@ -3,15 +3,25 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
+const pageRating = require('../models/ratingsmodel')
 
+router.get("/getRatings", async (req, res) => {
+    try {
+        const ratings = await pageRating.findOne({ page: 'home'}).exec();
+        res.status(200).json(ratings)
+    } catch (error){
+        res.status(400).json({error: error.message})
+    }
+})
 
-router.route("/getRatings").get(function(req, res) {
-    console.log("TEST")
-    let db_connect = dbo.getDb("website");
-    db_connect.collection("pageRating").find({}).toArray(function(err, result){
-        if (err) throw err;
-        res.json(result);
-    })
+router.post("/createInitialRatings", async (req, res) => {
+    const {page, numSmile, numNeutral, numFrown} = req.body
+    try {
+        const ratings = await pageRating.create({page, numSmile, numNeutral, numFrown})
+        res.status(200).json(ratings)
+    } catch (error){
+        res.status(400).json({error: error.message})
+    }
 })
 
 
--- mern/server/server.js
+++ mern/server/server.js
@@ -1,19 +1,32 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const express = require('express')
+const getRatingsRoutes = require('./routes/getRatings')
+const mongoose = require('mongoose')
+//express app
+const app = express()
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 3001;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-   });
-  // console.log(process.env.ATLAS_URI);
-  console.log(`Server is running on port: ${port}`);
-});
 
+port = process.env.PORT
+
+//middleware
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next()
+})
+
+//routes
+app.get('/', (req, res) => {
+  res.json({mssg: "Welcome to the app"})
+})
+app.use('/api/ratings', getRatingsRoutes)
+
+// connect to db
+mongoose.connect(process.env.ATLAS_URI).then(() => {
+  app.listen(port, () => {
+    console.log('connected to db and listening on ' + port)
+  })
+}).catch((error) => {
+  console.log(error)
+})
+
+// listen for request
