34a4c2209ecf02375eb5ee47eeea9ba6b0940dfa
= <=>
2023-11-04 00:18:21 -0400

    Chatbot Component

--- mern/client/src/Chatbox/chatbotComponent.jsx
+++ mern/client/src/Chatbox/chatbotComponent.jsx
@@ -1,13 +1,48 @@
-import React from "react";
+//import React from "react";
 import "./chatbot.css";
+import React, { useState, useEffect } from "react";
+import "./chatbot.css";
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
+
+//import { MainContainer, , MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react";
+import openai from 'openai';
 
 export default function ChatbotComponent() {
+
+    const [messages, setMessages] = useState([]);
+    const [userMessage, setUserMessage] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const handleInputSubmit = async () => {
+    if (!userMessage) return;
+
+    setIsLoading(true);
+
+    try {
+      // Make a request to the OpenAI GPT-3 API
+      const response = await openai.create({
+        engine: 'davinci',
+        prompt: userMessage,
+        max_tokens: 150,
+      });
+    
+      const botResponse = response.data.choices[0].text;
+
+      setMessages([...messages, { text: userMessage, type: "user" }, { text: botResponse, type: "bot" }]);
+    } catch (error) {
+      console.error("Error sending message to OpenAI GPT-3:", error);
+    }
+
+    setIsLoading(false);
+    setUserMessage(""); // Clear the input field
+  };
+
+
   return (
-    <div id="container" className="containerBox">
+    <div id="container" className="containerBox chatBotBox">
         <header className="Top">
             <div className="containerHeader">
                 <h1 className="Title">
-                   Goodwill Columbus Chatbot
+                 Goodwill Columbus Chatbot
                 </h1>
             </div>
         </header>   
@@ -46,17 +81,27 @@ export default function ChatbotComponent() {
                 </div>
             </div>
         </main>  
-        <footer className="Footer chatBotFooter">
-            <div className="TextBoxFormat">
-                <form className="Form">
-                <input autoComplete="off" type="text" name="Request Input" 
-                className="Form" placeholder="Type your question here..." style={{ width: '250px', marginTop: '100px' }}>
-                </input>
-                <button className="submitButton">
-                    Submit
-                </button>
-                </form>           
-            </div>
+        <footer className="Footer">
+        <ChatContainer>
+          <MessageList>
+            {messages.map((message, index) => (
+              <Message
+                key={index}
+                model={{
+                  message: message.text,
+                  sentTime: "just now",
+                  sender: message.type,
+                }}
+              />
+            ))}
+            {isLoading && <TypingIndicator content="AI is typing..." />}
+          </MessageList>
+          {<MessageInput
+            value={userMessage}
+            onSend={handleInputSubmit}
+            placeholder="Type your question here..."
+              />}
+        </ChatContainer>
         </footer>  
     </div>
   );
