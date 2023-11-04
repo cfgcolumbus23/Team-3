37cbfa638024324529dd4d6369e00f2ab6a7c670
ayushenagpal <ayushenagpal@gmail.com>
2023-11-04 02:56:28 -0400

    typing animation home page

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header.js'
 import Footer from './components/footer';
-import Home from "./pages/Home.js";
+import Home from "./pages/Home";
+import Courses from "./pages/Course"
+import Resources from "./pages/mentorship"
+import Mentorship from "./pages/mentorship"
 import Login from "./pages/Login"
 import Signup from './pages/Signup';
 
-//import Mentorship from "./pages/Mentorship.js"
 //import { BrowserRouter,Switch,Route, Routes, Outlet, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route, Outlet, Link,  BrowserRouter} from "react-router-dom";
@@ -31,12 +33,12 @@ function App() {
             <Header />
               <div className = "pages">
                 <Routes>
-                  <Route path = "/" element = {<Home />}
-                />
-                  <Route path = "/login" element = {<Login />}
-                />
-                <Route path = "/signup" element = {<Signup/>}
-                />
+                  <Route path = "/" element = {<Home />}/>
+                  <Route path = "/courses" element = {<Courses />} />
+                  <Route path = "/resources" element = {<Resources />} />
+                  <Route path = "/mentorship" element = {<Mentorship />} />
+                  <Route path = "/login" element = {<Login />} />
+                  <Route path = "/signup" element = {<Signup/>} />
                 </Routes>
               </div>
             <Footer />
