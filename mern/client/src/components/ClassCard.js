69e703de7555ed0167b504e4dba1273cc703b02d
Amber Lai <alai8@u.rochester.edu>
2023-11-03 22:57:45 -0400

    changed card content

--- mern/client/src/components/ClassCard.js
+++ mern/client/src/components/ClassCard.js
@@ -18,7 +18,7 @@ return (
               <CardText> {about} </CardText> 
               <CardText> {requirements} </CardText> 
               <CardText> {schedule} </CardText> 
-              <Popup trigger={<Button className="custom-btn" >Learn More!</Button> } modal>  
+              <Popup trigger={<Button className="custom-btn" >Learn More!</Button> } modal nested>  
               {close => <span> <Button onClick={close}>close</Button> <div className="header"><h3>{name}</h3></div><div className="content"><h5>{description}</h5></div> <p>{about}</p> <p>{requirements}</p> <p>{schedule}</p>
                 </span>}
               </Popup>
