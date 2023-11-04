dfcf823a7845f2d535e555ae7832c3be3c30e685
ayushenagpal <ayushenagpal@gmail.com>
2023-11-03 20:15:47 -0400

    nav bar routing

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -7,7 +7,6 @@ function App() {
     <>
        <BrowserRouter>
         <ResponsiveNavBar />
-
         <div className="pages">
           <Switch>
             <Route exact path="/" component={Home} />
