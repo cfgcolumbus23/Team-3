ffd3e0e09e23e09f73b9e6a511198d8691482b5d
Krish Sanghvi <krishksanghvi@gmail.com>
2023-11-03 23:59:54 -0400

    updated packages for routes

--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* <App />  */}
+      <Switch>  
+        <Route exact path="/" component={App} />
+        {/* <Route path="/page2" component={Page2} /> */}
+      </Switch>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
