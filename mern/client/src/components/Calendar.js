691c608bf3f0742d5ebf4960cf0ad69e1c99d3c2
Amber Lai <alai8@u.rochester.edu>
2023-11-04 07:36:32 -0400

    added google maps to tool tip

--- mern/client/src/components/Calendar.js
+++ mern/client/src/components/Calendar.js
@@ -66,18 +66,20 @@ class CourseCalendar extends Component {
                 start: moment('2023-11-10T10:00:00').toDate(),
                 end: moment('2023-11-10T12:00:00').toDate(),
                 title: "CML Barnett - Computer Basics",
-                location: "3434 E Livingston Ave, Columbus, OH 43227"
+                location: "3434 E Livingston Ave, Columbus, OH 43227", 
+                mapURL: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3058.7573157000857!2d-82.90696532409918!3d39.94681778410603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883862a052b4ca79%3A0x111cb4f1293b00b8!2s3434%20E%20Livingston%20Ave%2C%20Columbus%2C%20OH%2043227!5e0!3m2!1sen!2sus!4v1699085813578!5m2!1sen!2sus"
               },
               {
                 start: moment('2023-11-06T10:00:00').toDate(),
                 end: moment('2023-11-06T12:30:00').toDate(),
                 title: "Gahanna Branch",
-                location: "310 Hamilton Rd, Gahanna, OH 43230"
+                location: "310 Hamilton Rd, Gahanna, OH 43230",
+                mapURL: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3055.90519979295!2d-82.86914362409635!3d40.01056508026007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8838618105caa20d%3A0x514e351eed81d3a2!2s310%20S%20Hamilton%20Rd%2C%20Gahanna%2C%20OH%2043230!5e0!3m2!1sen!2sus!4v1699086260931!5m2!1sen!2sus"
               },
         
             ],
     showTooltip: false,
-    tooltipPosition: { top: 0, left: 0 },
+    // tooltipPosition: { top: 0, left: 0 },
     selectedEvent: null,
   };
 
--- mern/client/src/components/EventTooltip.js
+++ mern/client/src/components/EventTooltip.js
@@ -9,6 +9,16 @@ const EventTooltip = ({ event, onClose }) => {
       </button>
       <h3>{event.title}</h3>
       <p>{event.location}</p>
+    
+      <iframe
+        src={event.mapURL} // Add a mapUrl property to your event data
+        width="400"
+        height="300"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 };
