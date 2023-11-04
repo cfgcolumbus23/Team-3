4517c2bedea5146204ab6e4800e871fcbffa55b7
ayushenagpal <ayushenagpal@gmail.com>
2023-11-04 03:37:09 -0400

    fixes

--- mern/client/src/components/footer.js
+++ mern/client/src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import smile from '../smile.svg';
 import neutral from '../neutral.svg';
 import frown from '../frown.svg';
