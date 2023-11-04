432a48a611730c4c615964cb5e3e46d89ecc9817
Rajit Khatri <rajitkhtr@gmail.com>
2023-11-03 21:46:55 -0400

    Temp commit

--- mern/client/src/App.js
+++ mern/client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NewsFeed from "./NewsFeed"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <NewsFeed/>
       </header>
     </div>
   );
