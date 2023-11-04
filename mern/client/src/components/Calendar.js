deeb6f92e064aac5f789eaeba95b48f274fdd07f
Amber Lai <alai8@u.rochester.edu>
2023-11-04 00:31:43 -0400

    changed favicon, font, and title

--- mern/client/src/components/Calendar.js
+++ mern/client/src/components/Calendar.js
@@ -1,21 +1,45 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "./Calendar.css";
-function Calender() {
-    const [date, setDate] = useState(new Date());
-  
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./Calendar.css"
+const localizer = momentLocalizer(moment);
+
+class CourseCalendar extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "CML Barnett - Computer Basics"
+      },
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "Some title"
+      },
+
+    ]
+  };
+
+  render() {
     return (
-      <div>
+      <div className="CourseCalendar">
+        <h5>Course Calendar</h5>
         <Calendar
-            onChange={date => setDate(date)}
-            value={date}
-            tileContent={({ date, view }) => (
-                <div className="custom-date-style">
-                {date.getDate()}
-                </div>
-            )}
-            />
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
       </div>
     );
   }
-  export default Calender;
+}
+
+export default CourseCalendar;
