8aec70f845135e85ef23e620e4854669558078a8
Eric Wang <eric0717@gmail.com>
2023-11-03 22:40:47 -0400

    updates on backend

--- mern/server/routes/articles.js
+++ mern/server/routes/articles.js
@@ -4,9 +4,8 @@ const router = express.Router();
 
 
 router.get("/getArticles", async (req, res) => {
-    const {id} = req.query
     try {
-        const articles = await article.findById(id).exec();
+        const articles = await article.find().exec();
         res.status(200).json(articles)
     } catch (error){
         res.status(400).json({error: error.message})
