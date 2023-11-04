b8f7e9e47518ee188b5fad94f0205862b1e391b7
Krish Sanghvi <krishksanghvi@gmail.com>
2023-11-04 03:04:14 -0400

    package fixes

--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -4,17 +4,21 @@ import App from "./App";
 import Course from "./pages/Course";
 import Mentorship from "./pages/mentorship";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = {<App />}/>
-        <Route path = "/Courses" element = {<Course />} />
-        
-        <Route path = "/Mentorship" element = {<Mentorship />} />
-        
-      </Routes>
-    </BrowserRouter>  
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element = {<App />}/>
+          <Route path = "/Courses" element = {<Course />} />
+          
+          <Route path = "/Mentorship" element = {<Mentorship />} />
+          
+        </Routes>
+      </BrowserRouter>  
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
